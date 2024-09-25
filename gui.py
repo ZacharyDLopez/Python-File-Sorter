2e5c75040d3cd47cd2823b4cf7fe559dcb2e3855
Zachary Lopez <zacharydlopez22@gmail.com>
2024-09-24 23:06:00 -0400

    Added buttons along with the trying to create states for those buttons

--- gui.py
+++ gui.py
@@ -4,18 +4,25 @@ import pathlib
 
 def button_clicked():
    askdirectory()
+
+def path_entry():
+    pass
    
 def browseFiles():
     window = tk.Tk()
-    # window size, color, and title
+    # Define window size, color, and title
     window.title("File Sorter")
     window.geometry("700x500")
     window.configure(bg = "#2C2C2C")
 
-    # Textbox with file path
-    # path_name = askdirectory()
+    # Define textbox with file path
+    place_holder = "Please select a directory path"
+    path_entry = tk.Entry(window,
+                          textvariable=place_holder
+                          ,width=40
+                          ,font=('Roboto', 12,'bold'))
 
-    #Button Functionality
+    #Define button functionality
     browse_text = "Browse"
     browse_button = tk.Button(window, 
                    text=browse_text, 
@@ -24,20 +31,68 @@ def browseFiles():
                    activeforeground="white",
                    anchor="center",
                    bd=3,
-                   bg="lightgray",
+                   bg="#696969",
                    cursor="hand2",
                    disabledforeground="gray",
-                   fg="black",
+                   fg="#D4D4D4",
                    font=("Arial", 12),
-                   height=2,
-                   highlightbackground="black",
+                   height=1,
+                   highlightbackground="#444444",
                    highlightcolor="green",
                    highlightthickness=2,
                    justify="center",
                    overrelief="raised",
                    padx=10,
                    pady=5,
-                   width=15,
+                   width=10,
+                   wraplength=100)
+
+    sort_text = "Sort"
+    sort_button = tk.Button(window, 
+                   text=sort_text, 
+                   command=button_clicked,
+                   activebackground="blue", 
+                   activeforeground="white",
+                   anchor="center",
+                   bd=3,
+                   bg="#696969",
+                   cursor="hand2",
+                   disabledforeground="#323232",
+                   fg="#D4D4D4",
+                   font=("Arial", 12),
+                   height=1,
+                   highlightbackground="#444444",
+                   highlightcolor="green",
+                   highlightthickness=2,
+                   justify="center",
+                   overrelief="raised",
+                   padx=10,
+                   pady=5,
+                   width=10,
+                   wraplength=100)
+
+    open_folder_text = "Open Folder"
+    open_folder_button = tk.Button(window, 
+                   text=open_folder_text, 
+                   command=button_clicked,
+                   activebackground="blue", 
+                   activeforeground="white",
+                   anchor="center",
+                   bd=3,
+                   bg="#696969",
+                   cursor="hand2",
+                   disabledforeground="#323232",
+                   fg="#D4D4D4",
+                   font=("Arial", 12),
+                   height=1,
+                   highlightbackground="#444444",
+                   highlightcolor="green",
+                   highlightthickness=2,
+                   justify="center",
+                   overrelief="raised",
+                   padx=10,
+                   pady=5,
+                   width=10,
                    wraplength=100)
 
 
@@ -51,7 +106,6 @@ def browseFiles():
     tooltip_text = "Please select a folder to sort in Browse"
     
     # Text Functionality
-
     title_display = tk.Label(window, text = title)
     title_display.config(bg = "#2C2C2C", fg = "#D4D4D4",font=("Roboto", 40))
 
@@ -60,7 +114,12 @@ def browseFiles():
     
     title_display.place(relx = 0.33, rely = 0.1)
     subheading_display.place(relx= 0.33, rely=0.24)
-    browse_button.place(relx= 0.30,rely=0.30)
+    path_entry.place(relx = 0.22,rely = 0.40)
+    browse_button.place(relx= 0.22,rely=0.65)
+    sort_button.place(relx=0.42,rely= 0.65)
+    sort_button.config(state="disabled")
+    open_folder_button.place(relx=0.62,rely=0.65)
+    open_folder_button.config(state="disabled")
 
     window.mainloop()
 browseFiles()
